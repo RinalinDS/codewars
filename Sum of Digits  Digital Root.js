3eb87a26c2ed2ebe0d1e603ebade7f3d668b506a
RinalisDS <denispilyutin@gmail.com>
2022-04-29 15:21:00 +0300

    + 29/04

---
+++ Sum of Digits  Digital Root.js
@@ -0,0 +1,11 @@
+function digital_root(n) {
+  if (n.toString().length === 1) {
+    return n
+  }
+    return digital_root(Array.from(n.toString(), Number).reduce((acc, el) => acc + el , 0))
+
+
+
+}
+
+console.log(digital_root(8123))
