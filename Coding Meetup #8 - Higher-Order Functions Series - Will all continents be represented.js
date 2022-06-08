4c22eb04138e4b73a7c78ec2b1a7332613af6c94
RinalisDS <denispilyutin@gmail.com>
2022-06-08 10:07:24 +0300

    + 08 /06

---
+++ Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented.js
@@ -0,0 +1,15 @@
+function allContinents(list) {
+  return new Set(list.map(m => m.continent)).size === 5
+}
+
+
+
+const list1 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+
+console.log(allContinents(list1))
